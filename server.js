12b4d9324048f54456c3c1294cf5949c8d5a210d
Ed Carlos Moraes <edcarlos.moraes@outlook.com>
2022-09-12 22:00:49 -0300

    adjust server

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<atividade-pratica>'));
+app.use(express.static(__dirname + '/dist/atividade-pratica'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<atividade-pratica>/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/atividade-pratica/index.html'));});
 app.listen(process.env.PORT || 8080);
